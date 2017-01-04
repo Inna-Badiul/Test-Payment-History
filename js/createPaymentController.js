a57862e42ad42925e71515b8ab09bd2c27a9b5e2
Inna-Badiul <inna.badiul@gmail.com>
2017-01-04 16:50:28 +0100

    added proper form validation

--- js/createPaymentController.js
+++ js/createPaymentController.js
@@ -4,29 +4,41 @@ App.createPaymentController = Object.assign({
         this.render();
         this.addEvents();
     },
+
     render: function () {
         this.renderTemplate({});
     },
+
     create: function (e) {
         e.preventDefault();
         var description = $('input[name="description"]').val();
         var summ = $('input[name="summ"]').val();
-        this.validateForm(description,summ);
-        if(this.validateForm(description,summ)===true){
+        this.validateForm(description, summ);
+        if (this.validateForm(description, summ) === true) {
             App.paymentItemsStorage.addNew(description, summ);
             App.Router.setRoute('/');
         }
     },
+
     addEvents: function () {
         this.addEvent("submit", "form", this.create);
     },
-    validateForm: function(description,summ){
-        if(description.length<5){
+
+    descriptionValidationExp: /.{5,}/,
+    summValidationExp:  /^[\+\-]?\d+$/,
+
+    validateForm: function (description, summ) {
+        var isValid = true;
+        if (this.descriptionValidationExp.test(description) === false) {
             $('input[name="description"]').addClass("invalid");
             $(".hint-description").css("display", "block");
-            return false;
-        }else{
-            return true;
+            isValid = false;
         }
+        if (this.summValidationExp.test(summ) === false) {
+            $('input[name="summ"]').addClass("invalid");
+            $(".hint-summ").css("display", "block");
+            isValid = false;
+        }
+            return isValid;
     }
 }, App.baseController);
