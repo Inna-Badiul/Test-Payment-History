be9812e42b99a6afb7bc3d2a71d79192130f15a3
Inna-Badiul <inna.badiul@gmail.com>
2017-01-04 22:14:08 +0100

    finished work with form validation

--- js/createPaymentController.js
+++ js/createPaymentController.js
@@ -13,32 +13,51 @@ App.createPaymentController = Object.assign({
         e.preventDefault();
         var description = $('input[name="description"]').val();
         var summ = $('input[name="summ"]').val();
-        this.validateForm(description, summ);
-        if (this.validateForm(description, summ) === true) {
+        if (this.isDescriptionValid && this.isSummValid) {
             App.paymentItemsStorage.addNew(description, summ);
             App.Router.setRoute('/');
+        } else {
+            this.validateDescription();
+            this.validateSumm();
         }
     },
 
     addEvents: function () {
         this.addEvent("submit", "form", this.create);
+        this.addEvent("blur", 'input[name="description"]', this.validateDescription);
+        this.addEvent("blur", 'input[name="summ"]', this.validateSumm);
+
     },
 
     descriptionValidationExp: /.{5,}/,
-    summValidationExp:  /^[\+\-]?\d+$/,
-
-    validateForm: function (description, summ) {
-        var isValid = true;
+    isDescriptionValid: false,
+    validateDescription: function () {
+        var description = $('input[name="description"]').val();
         if (this.descriptionValidationExp.test(description) === false) {
             $('input[name="description"]').addClass("invalid");
-            $(".hint-description").css("display", "block");
-            isValid = false;
+            $(".hint-description").show();
+            this.isDescriptionValid = false;
+        } else {
+            $('input[name="description"]').removeClass("invalid");
+            $(".hint-description").hide();
+            this.isDescriptionValid = true;
         }
+
+    },
+
+    summValidationExp: /^[\+\-]?\d+$/,
+    isSummValid: false,
+    
+    validateSumm: function () {
+        var summ = $('input[name="summ"]').val();
         if (this.summValidationExp.test(summ) === false) {
             $('input[name="summ"]').addClass("invalid");
-            $(".hint-summ").css("display", "block");
-            isValid = false;
+            $(".hint-summ").show();
+            this.isSummValid = false;
+        } else {
+            $('input[name="summ"]').removeClass("invalid");
+            $(".hint-summ").hide();
+            this.isSummValid = true;
         }
-        return isValid;
     }
 }, App.baseController);
